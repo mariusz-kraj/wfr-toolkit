0b4e6f34f10af5ac523eff8ff282bd2a67b25bae
Mariusz Kraj <mariusz.kraj@gmail.com>
2015-06-07 23:46:15 +0200

    FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Entity\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
