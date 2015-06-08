9ff846f4e18876795ed26d179df8ccb0b46f5e1d
Mariusz Kraj <mariusz.kraj@gmail.com>
2015-06-08 14:57:42 +0200

    Added EasyAdmin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
 
             new AppBundle\AppBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
