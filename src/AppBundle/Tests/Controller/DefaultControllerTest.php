e47c8a2513d44b1584ed5fc7eac717e51f84bb86
Mariusz Kraj <mariusz.kraj@gmail.com>
2015-06-10 12:05:00 +0200

    Sensiolab insight

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,7 +13,7 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertGreaterThan(1, $crawler->filter('html:contains("Homepage")')->count());
+        $this->assertGreaterThanOrEqual(1, $crawler->filter('html:contains("Homepage")')->count());
     }
 
     public function seriesIndex()
